addcb6a6c9dd9b2767bd993d73617735b86240ed
Sean Buckley <sbuckley@andrew.cmu.edu>
2015-01-15 21:11:42 -0500

    Code running on the lost Arduino Micro

--- RocketBoard/RocketBoard.ino
+++ RocketBoard/RocketBoard.ino
@@ -1,245 +1,63 @@
 #include <SD.h>
-//#include <Adafruit_VC0706.h>
-#include <SPI.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
 #include "MPL3115A2.h"
-//#include <TimerOne.h>
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
+#include <Wire.h>
 
-
-//SoftwareSerial cameraconnection = SoftwareSerial(8,9);
-//Adafruit_VC0706 cam = Adafruit_VC0706(&cameraconnection);
 MPL3115A2 myPressure;
-
-MPU6050 mpu;
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-//Quaternion q;           // [w, x, y, z]         quaternion container
-//VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
+File myFile;
 
 void setup(){
+  Serial.begin(9600); //to be removed before flight;
+  //while (!Serial); //same as above
+  pinMode(13, OUTPUT);
   Wire.begin();
-  Serial.begin(9600);
-  while(!Serial);
-  //Timer1.initialize(3000000);
-  //Timer1.attachInterrupt(collectData);
   myPressure.begin();
   myPressure.setModeAltimeter();
-  myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
-  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
-  pinMode(10, OUTPUT);
-  SD.begin(10);
-  /*if(!SD.begin(10)){
-    Serial.println("Card failed!");
-  }*/
-  //else Serial.println("Looks good!");
-/*  if (cam.begin()) {
-    Serial.println("Camera Found:");
-  } else {
-    Serial.println("No camera found?");
+  myPressure.setOversampleRate(7);
+  myPressure.enableEventFlags();
+  digitalWrite(13, HIGH);
+  delay(500);
+  Serial.print("Initializing SD card...");
+  digitalWrite(13, LOW);
+  pinMode(10, OUTPUT); //chip select pin for sd card
+   
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    digitalWrite(13, HIGH);
     return;
-  }*/
-  
-  
-    Wire.begin();
-
-
-    // initialize device
-    //Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-
-    // verify connection
-    //Serial.println(F("Testing device connections..."));
-    //Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
-    //Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-    // make sure it worked (returns 0 if so)
-   /* if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-
-        // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }*/
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile){
+    Serial.print("Writing initial time stamp");
+    myFile.print("Start time: ");
+    myFile.println(millis());
+    myFile.close();
+  }
+   
 }
 
 void loop(){
-  collectData();
-  delay(500);
-}
-
-void collectData(){
-  Serial.print("asd");
-  int time = millis();
-  float temperature = myPressure.readTempF();
- // Serial.print(" Temp(f):");
-  //Serial.print(temperature, 2);
   
   float altitude = myPressure.readAltitudeFt();
-  //Serial.print(" Altitude(ft):");
-  //Serial.print(altitude, 2);
+  Serial.print(" Altitude(ft):");
+  Serial.print(altitude, 2);
+  float temperature = myPressure.readTempF();
+  
+  Serial.print(" Temp(f):");
+  Serial.print(temperature, 2);
 
-  //Serial.println(); 
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-  if (dataFile){
-    dataFile.print("Time: ");
-    dataFile.println(time);
-    dataFile.print("Temperature: ");
-    dataFile.println(temperature);
-    dataFile.print("Altitude: ");
-    dataFile.println(altitude);
-    dataFile.println("G: ");
-    
-    dataFile.println();
-    //Serial.println("Copied into SD card!");
+  Serial.println();
+  
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile){
+    Serial.print("Writing time stamp");
+    myFile.print("Current time: ");
+    myFile.println(millis());
+    myFile.print("Temperature: ");
+    myFile.println(temperature);
+    myFile.print("Altitude: ");
+    myFile.println(altitude);    
+    myFile.close();
   }
-  //else Serial.println("Error writing to SD card!");
-  dataFile.close();
-
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-
-    // reset interrupt flag and get INT_STATUS byte
-//    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        //Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-/*
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-*/
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-      /*  #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif*/
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-        #endif
-
-    }
   
 }
